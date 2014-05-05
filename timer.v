d120e4603efb92fcf0d5b47131deed5cb08baf77
fredthekid <fredthekid@gmail.com>
2014-05-04 18:54:54 -0700

    Update timer.v

--- timer.v
+++ timer.v
@@ -1,2 +1,7 @@
 module timer(
+        input wire clk,
+        
         );
+
+reg [26:0] secondDivideCount = 0; //after 99999999 100MHz counts, 1 second has passed
+reg incrementSecond = 0; //if 1, ones place for second needs to be incremented
