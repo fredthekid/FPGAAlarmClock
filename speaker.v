bdc21f90be207b12b2e897714d807c73e2cee85b
fredthekid <fredthekid@gmail.com>
2014-05-04 18:52:55 -0700

    Create speaker.v

---
+++ speaker.v
@@ -0,0 +1,6 @@
+module speaker(
+        input wire clk,         // main clk
+        output wire speaker,    // used for outputting speaker
+        output wire vcc         // powering the output
+        );
+        
