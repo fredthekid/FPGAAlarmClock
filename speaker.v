862c34c374375d7359f15c8da68b344158d659bd
fredthekid <fredthekid@gmail.com>
2014-05-04 18:59:20 -0700

    Update and rename speaker.v to alarm.v

--- speaker.v
+++
@@ -1,6 +0,0 @@
-module speaker(
-        input wire clk,         // main clk
-        output wire speaker,    // used for outputting speaker
-        output wire vcc         // powering the output
-        );
-        
